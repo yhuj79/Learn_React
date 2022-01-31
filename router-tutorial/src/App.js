8aa9fb78b99a7f20fa326ce757b4b48c0b34a742
yhuj79 <yu1085@naver.com>
2022-02-01 02:59:48 +0900

    22.02.01.commit.

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,24 @@
+import { Route, Link, Routes } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  )
+}
+export default App;
