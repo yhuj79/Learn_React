f336cabf0f95b3607dd19ff0c16dd2152cabd580
yhuj79 <yu1085@naver.com>
2022-03-25 10:23:04 +0900

    22.03.25.commit.

--- testprobability/src/App.js
+++ testprobability/src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import './Result';
-import TOTYTopPriceAll from './TOTYTopPriceAll';
+import TotyNormal from './TotyNormal';
+import TotyReinforce from './TotyReinforce';
+import Icon from './Icon';
 
 function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1>22TOTY Top Price All</h1>
-      <TOTYTopPriceAll />
+      <div className="App_card">
+        <TotyNormal />
+        <TotyReinforce />
+        <Icon />
+      </div>
     </div>
   );
 }
--- testprobability/src/Result.js
+++ testprobability/src/Result.js
@@ -1,35 +1,43 @@
 import React from 'react';
 import './Result.css';
-import bottomMark from './image/22TOTYbottom.png'
-import TOTY22mark from './image/22TOTYmark.png';
+import TotyMarkNormal from './image/TotyMarkNormal.png';
+import TotyMarkBottom from './image/TotyMarkBottom.png';
 
-function Result({ overall, position, nation, profile, reinforce, name, pay }) {
+function addComma(prNum) {
+    const regexp = /\B(?=(\d{3})+(?!\d))/g;
+    return prNum.toString().replace(regexp, ',');
+}
+
+function Result({ overall, position, nation, profile, reinforce, name, pay, price }) {
     return (
-        <div className="result_div">
-            <div className="result_top">
-                <p className="result_top_overall">{overall}</p>
-                <p className="result_top_position">{position}</p>
-                <img className="result_top_nation" alt="" src={nation} />
-            </div>
-            <img className="result_profileimg" alt="" src={profile} />
-            <div className="result_top_markdiv">
-                <img className="result_top_mark" alt="" src={TOTY22mark} />
-            </div>
-            {reinforce === 1 ? (
-                <div className="result_reinforcediv" style={{ background: '#4a4e55' }}>{reinforce}</div>
-            ) : (reinforce === 5
-                ? <div className="result_reinforcediv" style={{ background: '#C2C5CA' }}>{reinforce}</div>
-                : <div className="result_reinforcediv" style={{ background: '#C37653' }}>{reinforce}</div>
-            )}
-            <div className="result_bottom">
-                <div className="result_bottom_name">
-                    <img className="result_bottom_mark" alt="" src={bottomMark} />
-                    <p>&ensp;{name}</p>
+        <div>
+            <div className="result_div">
+                <div className="result_top">
+                    <p className="result_top_overall">{overall}</p>
+                    <p className="result_top_position">{position}</p>
+                    <img className="result_top_nation" alt="" src={nation} />
                 </div>
-                <div className="result_pay_hexagon">
-                    <p className="result_pay">{pay}</p>
+                <img className="result_profileimg" alt="" src={profile} />
+                <div className="result_top_markdiv">
+                    <img className="result_top_mark" alt="" src={TotyMarkNormal} />
+                </div>
+                {reinforce === 1 ? (
+                    <div className="result_reinforcediv" style={{ background: '#4a4e55' }}>{reinforce}</div>
+                ) : (reinforce === 5
+                    ? <div className="result_reinforcediv" style={{ background: '#C2C5CA' }}>{reinforce}</div>
+                    : <div className="result_reinforcediv" style={{ background: '#C37653' }}>{reinforce}</div>
+                )}
+                <div className="result_bottom">
+                    <div className="result_bottom_name">
+                        <img className="result_bottom_mark" alt="" src={TotyMarkBottom} />
+                        <p>&ensp;{name}</p>
+                    </div>
+                    <div className="result_pay_hexagon">
+                        <p className="result_pay">{pay}</p>
+                    </div>
                 </div>
             </div>
+            <p className="result_price">{addComma(price)} BP</p>
         </div>
     );
 }
