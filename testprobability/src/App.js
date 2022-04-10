5a6854bb6fcc1a844e72c9ca7109ccee0d1365e3
yhuj79 <yu1085@naver.com>
2022-04-11 08:09:57 +0900

    22.04.11.commit.

--- testprobability/src/App.js
+++ testprobability/src/App.js
@@ -11,7 +11,9 @@ function App() {
       <div className="App_header">
         <img className="App_gamelogo" alt="" src={GameLogo} />
         <img className="App_react" alt="logo" src={logo} />
+        <p className="App_release">22.04.01 기준가&emsp;/&emsp;Made by yhuj79</p> 
       </div>
+      
       <div className="App_card">
         <TotyNormal />
         <TotyReinforce />
