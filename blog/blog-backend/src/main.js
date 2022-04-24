928cc808f19a58cbf25e6734dfe1c03e8c71afd4
yhuj79 <yu1085@naver.com>
2022-04-25 02:43:54 +0900

    err

---
+++ blog/blog-backend/src/main.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+
+const api = require('./api');
+
+const {PORT, MONGO_URI} = process.env;
+
+mongoose
+.connect(MONGO_URI, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(e => {
+        console.error(e);
+    })
+
+const app = new Koa();
+const router = new Router();
+
+router.use('/api', api.routes());
+
+app.use(bodyParser());
+
+// app instance에 router 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+const port = PORT || 4000;
+app.listen(port, () => {
+    console.log('listening to port %d', port);
+});
