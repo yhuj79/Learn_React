26db7b3940faa51f9179e97eca370004bee6f002
yhuj79 <yu1085@naver.com>
2022-11-02 23:07:48 +0900

    expo-test add FlatList

--- app/expo-test/App.js
+++ app/expo-test/App.js
@@ -1,10 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Button,
+  TextInput,
+} from "react-native";
+import uuid from "react-uuid";
 
 export default function App() {
+  const [value, setValue] = useState({
+    key: "",
+    value: "",
+  });
+  const [list, setList] = useState([]);
+
+  function inputHandler(txt) {
+    setValue({ value: txt });
+  }
+  function addHandler() {
+    setValue({ key: uuid() });
+    setList((param) => {
+      return [...param, value];
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>EXPO TEST</Text>
+      <View style={styles.input}>
+        <TextInput
+          value={value.value}
+          onChangeText={inputHandler}
+          style={styles.textInput}
+          placeholder="Add List in Here"
+        />
+        <Button onPress={addHandler} title="ADD" />
+      </View>
+      <View style={styles.flatlist}>
+      <FlatList
+        data={list}
+        renderItem={(data) => (
+          <View style={styles.list} key={data.item.key}>
+            <Text style={styles.item}>{data.item.value}</Text>
+          </View>
+        )}
+      />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +55,33 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+  },
+  input:{
+    marginVertical: 80,
+    alignItems: "center",
+  },
+  textInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    width: 200,
+    height: 30,
+    textAlign: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+  },
+  flatlist: {
+    alignItems: "center",
+  },
+  list: {
+    width: 300,
+    backgroundColor: "lightblue",
+    marginBottom: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 7,
+    borderRadius: 7,
+  },
+  item: {
+    color: "darkblue",
   },
 });
